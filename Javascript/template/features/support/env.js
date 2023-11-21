a2e265ca2da624102b0633c9dd1123ae9e7b93b2
mark.sweetlove <mark.sweetlove@testevolve.com>
2023-11-21 16:50:31 +0000

    [MS] Undo BeforeAll hook

--- Javascript/template/features/support/env.js
+++ Javascript/template/features/support/env.js
@@ -1,8 +1,8 @@
 import { registerHooks } from "@testevolve/testevolve-spark";
-import { Before, BeforeAll, AfterStep, After, AfterAll, setDefaultTimeout } from "@cucumber/cucumber";
+import { Before, AfterStep, After, AfterAll, setDefaultTimeout } from "@cucumber/cucumber";
 import * as pages from "../support/pages";
 
-registerHooks({ Before, BeforeAll, AfterStep, After, AfterAll });
+registerHooks({ Before, AfterStep, After, AfterAll });
 
 // TODO:cucumber timeout, we cant set this from within the project or te, this is only needed during the browserstack integration
 // due to the tests taking slightly longer to run (5 seconds is the default timeout used by cucumber)
