3787a7ff1fa1ac8c5890e5c71cda98944b21d1b3
james readhead <james.readhead@e2x.com>
2023-11-22 11:39:25 +0000

    [JR] fixing example JS tests

--- Javascript/demos/browser/features/step_definitions/testEvolveSteps.js
+++ Javascript/demos/browser/features/step_definitions/testEvolveSteps.js
@@ -1,6 +1,5 @@
 import { Given, When, Then } from "@cucumber/cucumber";
 import { loginPage } from "../support/pages";
-import { testEvolve } from "@testevolve/testevolve-spark";
 
 Given(/^we navigate to the TestEvolve homepage$/, async () => {
   await loginPage.open();
@@ -13,10 +12,9 @@ When(/^we enter our credentials$/, async () => {
 });
 
 Then(/^an error message is displayed$/, async () => {
-  await loginPage.verifyError('Username, password or domain is incorrect. Please check your details.');
+  await loginPage.verifyError('Incorrect organisation, username or password.');
 });
 
 Then(/^an incorrect error message is displayed$/, async () => {
-  await testEvolve.visualCheck('test');
-  await loginPage.verifyError('Username or password is incorrect');
+  await loginPage.verifyError('Please check your details.');
 });
