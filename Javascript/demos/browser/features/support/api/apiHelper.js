88407c3a06cd057bcfdfa8135aacb7d867d872fa
james readhead <james.readhead@e2x.com>
2025-01-31 13:48:10 +0000

    [JR] fix tabs

--- Javascript/demos/browser/features/support/api/apiHelper.js
+++ Javascript/demos/browser/features/support/api/apiHelper.js
@@ -2,72 +2,72 @@ import axios from 'axios';
 import {testEvolve} from "@testevolve/testevolve-spark";
 
 export const findArticles = async (searchTerm = "new orleans") => {
-	let headers = {
-		'Accept': 'application/json',
-		'Content-Type': 'application/json'
-	};
+  let headers = {
+    'Accept': 'application/json',
+    'Content-Type': 'application/json'
+  };
 
-	try {
-		let config = {
-			method: 'get',
-			maxBodyLength: Infinity,
-			url: `https://chroniclingamerica.loc.gov/search/titles/results/?terms=${searchTerm}&format=json`,
-			headers: headers
-		};
+  try {
+    let config = {
+      method: 'get',
+      maxBodyLength: Infinity,
+      url: `https://chroniclingamerica.loc.gov/search/titles/results/?terms=${searchTerm}&format=json`,
+      headers: headers
+    };
 
-		const response = await axios.request(config);
-		testEvolve.log.info('API RESPONSE CODE: ', response.status);
-		const data = response.data;
-		testEvolve.log.info('API RESPONSE BODY: ', data)
-		return data;
-	} catch (error) {
-		throw new Error(error);
-	}
+    const response = await axios.request(config);
+    testEvolve.log.info('API RESPONSE CODE: ', response.status);
+    const data = response.data;
+    testEvolve.log.info('API RESPONSE BODY: ', data)
+    return data;
+  } catch (error) {
+    throw new Error(error);
+  }
 }
 
 export const assertResultsMatch = (responseData, searchTerm) => {
-	const normalise = (str) =>
-		str.toLowerCase().replace(/[^a-z0-9\s]/g, '').trim();
+  const normalise = (str) =>
+    str.toLowerCase().replace(/[^a-z0-9\s]/g, '').trim();
 
-	const normalisedSearchTerm = normalise(searchTerm);
+  const normalisedSearchTerm = normalise(searchTerm);
 
-	if (!responseData.items || !Array.isArray(responseData.items)) {
-		throw new Error('Invalid response data: "items" array not found.');
-	}
+  if (!responseData.items || !Array.isArray(responseData.items)) {
+    throw new Error('Invalid response data: "items" array not found.');
+  }
 
-	const failingItems = responseData.items.filter((item) => {
-		const normalisedTitle = normalise(item.title);
-		return !normalisedTitle.includes(normalisedSearchTerm);
-	});
+  const failingItems = responseData.items.filter((item) => {
+    const normalisedTitle = normalise(item.title);
+    return !normalisedTitle.includes(normalisedSearchTerm);
+  });
 
-	if (failingItems.length > 0) {
-		const failingTitles = failingItems
-			.map((item) => `- ${item.title}`)
-			.join('\n');
-		throw new Error(
-			`The following titles do not contain the search term "${searchTerm}":\n${failingTitles}`
-		);
-	}
+  if (failingItems.length > 0) {
+    const failingTitles = failingItems
+      .map((item) => `- ${item.title}`)
+      .join('\n');
+    throw new Error(
+      `The following titles do not contain the search term "${searchTerm}":\n${failingTitles}`
+    );
+  }
 };
 
 export const exampleDataOutputsFromResponse = (responseData) => {
-	if (!responseData.items || responseData.items.length === 0) {
-		testEvolve.log.warn("No articles found in response.");
-		return;
-	}
+  if (!responseData.items || responseData.items.length === 0) {
+    testEvolve.log.warn("No articles found in response.");
+    return;
+  }
 
-	if (responseData.items[0]) {
-		testEvolve.log.info('Extract the first object: ', responseData.items[0]);
-		testEvolve.log.info('Extract the "start_year" of the first object: ', responseData.items[0]['start_year']);
-		testEvolve.log.info('Extract the first "holding_type" of the first object: ', responseData.items[0]['holding_type'][0]);
-	}
+  if (responseData.items[0]) {
+    testEvolve.log.info('Extract the first object: ', responseData.items[0]);
+    testEvolve.log.info('Extract the "start_year" of the first object: ', responseData.items[0]['start_year']);
+    testEvolve.log.info('Extract the first "holding_type" of the first object: ', responseData.items[0]['holding_type'][0]);
+  }
 
-	if (responseData.items[1]) {
-		testEvolve.log.info('Extract the second object: ', responseData.items[1]);
-	}
+  if (responseData.items[1]) {
+    testEvolve.log.info('Extract the second object: ', responseData.items[1]);
+  }
 
-	if (responseData.items[2]) {
-		testEvolve.log.info('Extract the "url" of the third object: ', responseData.items[2]['url']);
-		testEvolve.log.info('Extract the second "holding_type" of the third object: ', responseData.items[2]['holding_type'][1]);
-	}
+  if (responseData.items[2]) {
+    testEvolve.log.info('Extract the "url" of the third object: ', responseData.items[2]['url']);
+    testEvolve.log.info('Extract the second "holding_type" of the third object: ', responseData.items[2]['holding_type'][1]);
+  }
 };
--- Typescript/demos/browser/features/step_definitions/apiTestSteps.ts
+++ Typescript/demos/browser/features/step_definitions/apiTestSteps.ts
@@ -2,11 +2,11 @@ import {Given, Then} from "@cucumber/cucumber";
 import * as apiHelper from '../support/api/apiHelper';
 
 Given(/^I search for news articles that reference '(.*)'$/, async function (data) {
-	this.searchTerm = data
-	this.results = await apiHelper.findArticles(this.searchTerm);
+  this.searchTerm = data
+  this.results = await apiHelper.findArticles(this.searchTerm);
 });
 
 Then(/^I receive results that are relevant to my search term$/, async function () {
-	apiHelper.exampleDataOutputsFromResponse(this.results);
-	apiHelper.assertResultsMatch(this.results, this.searchTerm);
+  apiHelper.exampleDataOutputsFromResponse(this.results);
+  apiHelper.assertResultsMatch(this.results, this.searchTerm);
 });
