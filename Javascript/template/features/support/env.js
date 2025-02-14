881d303f8d5de9e175d6f8673a2e6d252ce8f54c
andybird-te <andy.bird@testevolve.com>
2025-02-14 14:16:31 +0000

    js_scrollIntoViewPort and default timeout bump

--- Javascript/template/features/support/env.js
+++ Javascript/template/features/support/env.js
@@ -6,7 +6,7 @@ registerHooks({ Before, BeforeAll, AfterStep, After, AfterAll });
 
 // TODO:cucumber timeout, we cant set this from within the project or te, this is only needed during the browserstack integration
 // due to the tests taking slightly longer to run (5 seconds is the default timeout used by cucumber)
-setDefaultTimeout(20 * 1000);
+setDefaultTimeout(90 * 1000);
 
 Before(async () => {
   Object.values(pages).forEach(page => {
