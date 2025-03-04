00e50a614cf28219935a5493eb3e9a3382603136
Patrick Skipworth <patrick.skipworth@testevolve.com>
2025-03-04 09:20:05 +0000

    Python examples fixes (#56)
    * Add types to ts apiHelper example project file
    
    * Update homepage.py and set cookies handler logging to debug
    
    * Add behave import steps to python step file
    
    * Add additional necessary config to default config
    
    * Add linter configs. Add Python project default README. Update Ruby linter configs.
    
    * Update eslint config
    
    * Update python readme instructions
    
    * Add after_step handling to handle error logging
    
    * Update Readme instructions
    
    * Update Readme instructions
    
    * Add scroll to example test
    
    * Update examples to use PageObject
    
    * Refactor HomePage class to use refactored element syntax
    
    * Refactor homepage page file to reinstate double_click and select_value methods

--- Python/demos/browser/features/environment.py
+++ Python/demos/browser/features/environment.py
@@ -9,6 +9,10 @@ def before_all(context):
 
 def before_scenario(context, scenario):
     hooks.before_scenario(context, scenario)
+    
+
+def after_step(context, step):
+    hooks.after_step(context, step)
 
 
 def after_scenario(context, scenario):
